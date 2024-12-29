be7e240219d896204ae127b973b1a300b0e0efea
Steven <hensteve250@gmail.com>
2024-12-29 01:50:14 -0500

    cambiando display grid

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -92,7 +92,7 @@ const ImageGallery = (): React.ReactElement => {
           <p className="text-lg text-gray-500">No se han generado imágenes aún.</p>
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
           {images.map((image) => (
             <div
               key={image.id}
